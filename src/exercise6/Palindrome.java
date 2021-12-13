ce282d86c92b89d956dc0dc365295036788f19f1
AlexanderYkmv <sasho.yakimov@gmail.com>
2021-12-13 14:08:25 +0200

    ubung6

--- src/exercise6/Palindrome.java
+++ src/exercise6/Palindrome.java
@@ -4,6 +4,23 @@ import java.util.Scanner;
 
 public class Palindrome {
     public static void main(String[] args) {
+
         Scanner input = new Scanner(System.in);
+        int reversedNumber = 0;
+        int manipulatedNumber;
+        int digit;
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+        manipulatedNumber = number;
+        while(manipulatedNumber > 0) {
+            digit = manipulatedNumber % 10;
+
+            reversedNumber = reversedNumber * 10 + digit;
+            manipulatedNumber = manipulatedNumber / 10;
+        }
+        boolean palindrome = number == reversedNumber;
+        System.out.println("Palindrome: " + palindrome);
+
+
     }
 }
--- src/exercise6/PasswordCheck.java
+++ src/exercise6/PasswordCheck.java
@@ -5,5 +5,35 @@ import java.util.Scanner;
 public class PasswordCheck {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        boolean passwordValid = false;
+
+        do {
+            System.out.println("Enter password:");
+            String password = input.nextLine();
+            if (password.length() < 8) {
+                continue;
+            }
+            int digitCounter = 0;
+            boolean allDigitsNumbers = true;
+            for (int i = 0; i < password.length(); i++) {
+                char symbol = password.charAt(i);
+                if (!Character.isLetterOrDigit(symbol)) {
+                    allDigitsNumbers = false;
+                    break;
+                }
+                boolean digit = Character.isDigit(symbol);
+                if (digit) {
+                    digitCounter++;
+                }
+
+                }
+            if (allDigitsNumbers && digitCounter >= 2) {
+                passwordValid = true;
+            }
+
+
+        } while (!passwordValid);
+        System.out.println("Password valid!");
+
     }
 }
